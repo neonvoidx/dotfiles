6dcb27b6066d1eb9ca04fdd07cb44aef5a8ebc4d
jrreed <jacob.reed@oracle.com>
2025-11-06 08:45:38 -0500

    up

--- .config/sketchybar/items/spaces.lua
+++ .config/sketchybar/items/spaces.lua
@@ -24,86 +24,90 @@ end
 
 sbar.exec("aerospace list-workspaces --all", function(spaces)
   for space_name in spaces:gmatch "[^\r\n]+" do
-    local space = sbar.add("item", "space." .. space_name, {
-      icon = {
-        string = space_name,
-        color = colors.icon.color,
-        highlight_color = colors.icon.highlight,
-        padding_left = 8,
-      },
-      label = {
-        font = "sketchybar-app-font:Regular:14.0",
-        string = "",
-        color = colors.label.color,
-        highlight_color = colors.label.highlight,
-        y_offset = -1,
-      },
-      click_script = "aerospace workspace " .. space_name,
-      padding_left = space_name == "1" and 0 or 4,
-    })
-
-    add_windows(space, space_name)
-
-    space:subscribe("aerospace_workspace_change", function(env)
-      local selected = env.FOCUSED_WORKSPACE == space_name
-      space:set {
-        icon = { highlight = selected },
-        label = { highlight = selected },
-      }
-
-      if selected then
-        sbar.animate("tanh", 8, function()
-          space:set {
-            background = {
-              shadow = {
-                distance = 0,
-              },
-            },
-            y_offset = -4,
+    sbar.exec("aerospace list-windows --workspace " .. space_name, function(windows)
+      if windows and windows:match "%S" then
+        local space = sbar.add("item", "space." .. space_name, {
+          icon = {
+            string = space_name,
+            color = colors.icon.color,
+            highlight_color = colors.icon.highlight,
             padding_left = 8,
-            padding_right = 0,
-          }
+          },
+          label = {
+            font = "sketchybar-app-font:Regular:14.0",
+            string = "",
+            color = colors.label.color,
+            highlight_color = colors.label.highlight,
+            y_offset = -1,
+          },
+          click_script = "aerospace workspace " .. space_name,
+          padding_left = space_name == "1" and 0 or 4,
+        })
+
+        add_windows(space, space_name)
+
+        space:subscribe("aerospace_workspace_change", function(env)
+          local selected = env.FOCUSED_WORKSPACE == space_name
           space:set {
-            background = {
-              shadow = {
-                distance = 4,
-              },
-            },
-            y_offset = 0,
-            padding_left = 4,
-            padding_right = 4,
+            icon = { highlight = selected },
+            label = { highlight = selected },
           }
+
+          if selected then
+            sbar.animate("tanh", 8, function()
+              space:set {
+                background = {
+                  shadow = {
+                    distance = 0,
+                  },
+                },
+                y_offset = -4,
+                padding_left = 8,
+                padding_right = 0,
+              }
+              space:set {
+                background = {
+                  shadow = {
+                    distance = 4,
+                  },
+                },
+                y_offset = 0,
+                padding_left = 4,
+                padding_right = 4,
+              }
+            end)
+          end
+        end)
+
+        space:subscribe("space_windows_change", function()
+          add_windows(space, space_name)
+        end)
+
+        space:subscribe("mouse.clicked", function()
+          sbar.animate("tanh", 8, function()
+            space:set {
+              background = {
+                shadow = {
+                  distance = 0,
+                },
+              },
+              y_offset = -4,
+              padding_left = 8,
+              padding_right = 0,
+            }
+            space:set {
+              background = {
+                shadow = {
+                  distance = 4,
+                },
+              },
+              y_offset = 0,
+              padding_left = 4,
+              padding_right = 4,
+            }
+          end)
         end)
       end
-    end)
-
-    space:subscribe("space_windows_change", function()
-      add_windows(space, space_name)
-    end)
-
-    space:subscribe("mouse.clicked", function()
-      sbar.animate("tanh", 8, function()
-        space:set {
-          background = {
-            shadow = {
-              distance = 0,
-            },
-          },
-          y_offset = -4,
-          padding_left = 8,
-          padding_right = 0,
-        }
-        space:set {
-          background = {
-            shadow = {
-              distance = 4,
-            },
-          },
-          y_offset = 0,
-          padding_left = 4,
-          padding_right = 4,
-        }
-      end)
     end)
   end
 end)
