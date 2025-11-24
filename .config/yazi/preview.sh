da6406a9c1eaba403dcac8a8847a25f5bb8f10f6
neonvoidx <me@neonvoid.dev>
2025-11-23 19:41:11 -0500

    Updates to yazi

--- .config/yazi/preview.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-FILE="$1"
-
-if command -v identify >/dev/null; then
-  # Get width×height and format
-  identify -format "Format: %m\nDimensions: %wx%h\nSize: %b\n" "$FILE"
-else
-  echo "ImageMagick not installed – cannot fetch dimensions."
-fi
