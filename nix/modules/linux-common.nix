d8bad41ae1392710a0ab001a38456b2b95ac5fc8
neonvoidx <me@neonvoid.dev>
2025-11-29 21:40:46 -0500

    up

--- nix/modules/linux-common.nix
+++ nix/modules/linux-common.nix
@@ -1,26 +1,24 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [ 
-    /etc/nixos/hardware-configuration.nix
-    ./fonts.nix
-  ];
+  imports = [ /etc/nixos/hardware-configuration.nix ./fonts.nix ];
 
   # Bootloader
   boot = {
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
-     };
+    };
     plymouth = {
       enable = true;
       theme = "connect";
-      themePackages = with pkgs; [
-        # By default we would install all themes
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "connect" ];
-        })
-      ];
+      themePackages = with pkgs;
+        [
+          # By default we would install all themes
+          (adi1090x-plymouth-themes.override {
+            selected_themes = [ "connect" ];
+          })
+        ];
     };
 
     # Enable "Silent boot"
@@ -37,7 +35,7 @@
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
     loader.timeout = 0;
-   };
+  };
 
   time.timeZone = "America/New_York";
 
@@ -89,6 +87,9 @@
     protonup-ng
     protonmail-bridge
     thunderbird
+    spotify
+    spicetify-cli
+    # TODO scopebuddy
   ];
 
   environment.sessionVariables = {
